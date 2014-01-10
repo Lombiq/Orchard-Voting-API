70f83522aadb14b7741b43deea42bad6cb071622
Sipke Schoorstra <sipke@ideliverable.com>
2014-01-10 22:41:05 +0100

    Fixed references, updated project file to .NET 4.5 and cleanup.

--- Services/DefaultVotingService.cs
+++ Services/DefaultVotingService.cs
@@ -110,10 +110,6 @@ namespace Contrib.Voting.Services {
         }
 
         public static string GetCacheKey(int contentItemId, string function, string dimension) {
-
-            // (wmeints) Important: Include function in the cache key, otherwise
-            // the values for all functions are stored in the same cache item
-            // resulting in an invalid result value returned by the service.
             return String.Concat("vote_", contentItemId, "_", function, "_", dimension ?? "");
         }
     }
